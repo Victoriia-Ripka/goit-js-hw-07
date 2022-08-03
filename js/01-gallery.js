93720fc5b6613ab419fe9b5ed0648ae2225b8ace
Victoriia-Ripka <vikaripka@gmail.com>
2022-08-03 17:17:12 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,35 +9,33 @@ galleryDiv.addEventListener('click', onClick);
 function createCardsMarkUp(items) {
     return items.map(({ preview, original, description }) => {
         return `<div class="gallery__item">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
+    <a class="gallery__link" href="${original}"> 
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
 </div>`
     }).join('');
 }
-//href="${original}" => "${preview}"
 
 function onClick(event) {
+    event.preventDefault();
     if (event.target.nodeName !== 'IMG') {
         return;
     }
-    console.log(event.target.dataset.source);
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+    instance.show()
+
+    window.addEventListener("keydown", onEscClose);
+
+    function onEscClose(event) {
+        if (event.code === "Escape") {
+            window.removeEventListener("keydown", onEscClose);
+            instance.close();
+        }
+    }
 }
-
-//   <a class="gallery__link" href="${original}"> 
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-
-//Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач 
-//буде перенаправлений на іншу сторінку.Заборони цю поведінку за замовчуванням.
-//Додай закриття модального вікна після натискання клавіші Escape. 
-//Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
-//Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
