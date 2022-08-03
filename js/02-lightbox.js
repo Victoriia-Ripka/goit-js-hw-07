597e48dbd750b01eca290bbe269b73054ba784d0
Victoriia-Ripka <vikaripka@gmail.com>
2022-08-03 18:44:05 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const cardsMarkUp = createCardsMarkUp(galleryItems);
+
+galleryDiv.insertAdjacentHTML('beforeend', cardsMarkUp);
+galleryDiv.addEventListener('click', onClick);
+
+function createCardsMarkUp(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`}).join('');
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    let gallery = new SimpleLightbox('.gallery__item', {
+        captionDelay: 250,
+        captionsData: "alt",
+    });
+    gallery.on('show.simplelightbox');
+}
