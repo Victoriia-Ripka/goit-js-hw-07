5df9d4e20878dfa5034a7fc5cd2d0ab19eade075
Victoriia-Ripka <vikaripka@gmail.com>
2022-08-03 14:13:26 +0200

    basicLightbox + add cards

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const cardsMarkUp = createCardsMarkUp(galleryItems);
+
+galleryDiv.insertAdjacentHTML('beforeend', cardsMarkUp);
+galleryDiv.addEventListener('click', onClick);
+
+function createCardsMarkUp(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+</div>`
+    }).join('');
+}
+//href="${original}" => "${preview}"
+
+function onClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    console.log(event.target.dataset.source);
+}
+
+//   <a class="gallery__link" href="${original}"> 
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+
+//Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач 
+//буде перенаправлений на іншу сторінку.Заборони цю поведінку за замовчуванням.
+//Додай закриття модального вікна після натискання клавіші Escape. 
+//Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
+//Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
